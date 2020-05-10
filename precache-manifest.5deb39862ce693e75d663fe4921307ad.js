4ec7993dbcd3332943f2f7824b81e7d8392669cb
loctary <magic.loctary@gmail.com>
2020-05-10 16:34:21 +0300

    Updates

---
+++ precache-manifest.5deb39862ce693e75d663fe4921307ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3701a1b32dff25faaaed09e06e4c9f9",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "0928f7dca07623e0951e",
+    "url": "/birthday/static/css/main.ba2bdf9f.chunk.css"
+  },
+  {
+    "revision": "10c5a4cca00dc214810a",
+    "url": "/birthday/static/js/2.460a9e5a.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/birthday/static/js/2.460a9e5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0928f7dca07623e0951e",
+    "url": "/birthday/static/js/main.dc79858b.chunk.js"
+  },
+  {
+    "revision": "909509ed176f5faec88f",
+    "url": "/birthday/static/js/runtime-main.4fc00cde.js"
+  },
+  {
+    "revision": "afef165ecb0ba3c624b3633b3d3a407d",
+    "url": "/birthday/static/media/sad_kitty.afef165e.jpg"
+  }
+]);
